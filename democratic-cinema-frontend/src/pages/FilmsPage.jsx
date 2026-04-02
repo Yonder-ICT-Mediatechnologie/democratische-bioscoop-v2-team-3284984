720401cbd1717c37ebb6732c302e51dbb22122c0
Dark-StarStudios <146312613+Dark-StarStudios@users.noreply.github.com>
2026-04-02 19:45:42 +0200

    fix env

--- democratic-cinema-frontend/src/pages/FilmsPage.jsx
+++ democratic-cinema-frontend/src/pages/FilmsPage.jsx
@@ -21,7 +21,7 @@ const [movies, setMovies] = useState([]);
 
 async function fetchMovies() {
   try {
-    const response = await fetch(process.env.BACKEND_API_URL + '/films');
+    const response = await fetch(import.meta.env.VITE_BACKEND_API_URL + '/films');
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
